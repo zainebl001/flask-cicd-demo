480f7ddfa35bfe528ad259dfdd1d556d76cd8780
zainebl001 <166256158+zainebl001@users.noreply.github.com>
2025-11-02 22:44:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
 @app.route("/")
+
 def home():
-return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1><p>Automatic Build → Test → Deploy via
+  return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1><p>Automatic Build → Test → Deploy via
 GitHub Actions.</p>"
+
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", port=8080)
