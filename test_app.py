e34d01b02922e6d0e237703592ef8b877ec9e9e1
zainebl001 <166256158+zainebl001@users.noreply.github.com>
2025-11-02 23:16:15 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import app
 
 
 def test_home():
-  client = app.test_client()
-  res = client.get("/")
-  assert res.status_code == 200
-  assert b"Hello" in res.data
+    client = app.test_client()
+    res = client.get("/")
+    assert res.status_code == 200
+    assert b"Hello" in res.data
